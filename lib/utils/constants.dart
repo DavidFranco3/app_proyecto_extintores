5712c05572f2361ce473c5a5ea15fe6f6460f0c6
David Franco <josedavidayalafranco3@gmail.com>
2025-03-24 18:00:19 -0600

    .

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,16 +2,19 @@
 // const String API_HOST = "http://192.168.1.65:5050";
 
 // Constantes para puesta en producción
-const String API_HOST = 'https://apiaggo.isotech.mx/';
+const String API_HOST = 'https://apiaggo.isotech.mx';
 
 // API para la consulta de la IP pública
 const String API_IP = 'https://api.ipify.org';
 
+// Refresh token para obtener tokens para realizar acciones en dropbox
 String dropboxRefreshToken =
     'FH06TXz4MBcAAAAAAAAAAWgX7W7tnxYDkaWRqbsJN2nQ1l9fdxgysmSdrY_6wJsz';
 
+// API para cargar imagenes en dropbox
 const String API_DROPBOX = 'https://content.dropboxapi.com/2/files/upload';
 
+// API para obtener el enlace de imagenes cargadas en dropbox
 const String API_DROPBOX_ENLACE =
     'https://api.dropboxapi.com/2/sharing/create_shared_link_with_settings';
 
